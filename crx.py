8ca5f2c77c46c41f4f5f37a9cd70b915e0b8e940
Greyh4t <Greyh4t1337@hotmail.com>
2014-07-04 20:40:33 +0800

    Bug fixes

--- crx.py
+++ crx.py
@@ -4,6 +4,16 @@ import urllib, urllib2, sys, re, os
 user_agent = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.3 Safari/537.36'
 proxy = ''
 
+def savefile():
+    crx = open(unicode(path + '/' + crx_name + crx_version, 'utf-8'), 'wb')
+    crx.write(file)
+    crx.close()
+
+def over():
+    print 'Press the Enter key to exit'
+    raw_input()
+    sys.exit()
+
 path = sys.path[0]
 if os.path.isfile(path):
     path = os.path.dirname(path)
@@ -16,16 +26,14 @@ if len(sys.argv) < 2:
     or:
     python crx.py dpplabbmogkhghncfbfdeeokoefdjegm 127.0.0.1:8087
     """
-    print 'Press the Enter key to exit'
-    raw_input()
-    sys.exit()
+    over()
 
 if len(sys.argv) == 3:
     proxy = sys.argv[2]
 target = sys.argv[1]
 
 if 'chrome.google.com' in target:
-    tmp = re.findall(r'detail/(.*?)/(\w+)', target)
+    tmp = re.findall(r'detail/(.*?)/(\w+)\??', target)
     if not tmp:
         print """
     Please use a correct url or extension ID.
@@ -35,9 +43,7 @@ if 'chrome.google.com' in target:
     or:
     dpplabbmogkhghncfbfdeeokoefdjegm
     """
-        print 'Press the Enter key to exit'
-        raw_input()
-        sys.exit()
+        over()
     crx_name = tmp[0][0]
     crx_id = tmp[0][1]
 elif re.match('^[a-z]+$', target):
@@ -52,14 +58,13 @@ else:
     or:
     dpplabbmogkhghncfbfdeeokoefdjegm
     """
-    print 'Press the Enter key to exit'
-    raw_input()
-    sys.exit()
+    over()
 
 if '%' in crx_name:
     crx_name = urllib.unquote(crx_name)
 
-download_url = 'https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D' + crx_id + '%26uc'
+download_url = 'https://clients2.google.com/service/update2/crx?response=redirect&prodversion=37.0.2062.3&x=id%3D' + crx_id + '%26uc'
+
 
 if proxy:
     opener = urllib2.build_opener(urllib2.ProxyHandler({'https':proxy}))
@@ -78,13 +83,18 @@ except Exception, e:
     print 'Download failed'
     if not proxy:
         print 'Please try to use a proxy'
-    print 'Press the Enter key to exit'
-    raw_input()
-    sys.exit()
+    over()
 
-crx = open(unicode(path + '/' + crx_name + crx_version, 'utf-8'), 'wb')
-crx.write(file)
-crx.close()
+try:
+    savefile()
+except Exception, e:
+    if "'utf8' codec can't decode byte" in str(e):
+        crx_name = crx_name.decode('gbk').encode('utf-8')
+        savefile()
+    else:
+        print e
+        print 'Download failed'
+        over()
+
 print 'Download success to ' + unicode(path + '/' + crx_name + crx_version, 'utf-8')
-print 'Press the Enter key to exit'
-raw_input()
+over()
